49015188943a25fd42bb31645b9a86b6a6f7966d
Andrey <126459924+IIIMEL-1@users.noreply.github.com>
2024-07-10 21:22:06 +0700

    feat: fix bug Pages #2

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -2,36 +2,36 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
 
-import Main from "src/Pages/Main/Main";
+import Main from "@pages/Main/Main";
 
-import PageError from "src/Pages/Error/Error";
+import PageError from "@pages/Error/Error";
 
 // ProfilePage
-import PageProfile from "src/Pages/PersonalAccount/Profile/Profile";
-import PageMyOrders from "src/Pages/PersonalAccount/MyOrders/MyOrders";
-import PagePasswordChange from "src/Pages/PersonalAccount/PasswordChange/PasswordChange";
-import PagePersonalAccount from "src/Pages/PersonalAccount/PersonalAccount";
+import PageProfile from "@pages/PersonalAccount/Profile/Profile";
+import PageMyOrders from "@pages/PersonalAccount/MyOrders/MyOrders";
+import PagePasswordChange from "@pages/PersonalAccount/PasswordChange/PasswordChange";
+import PagePersonalAccount from "@pages/PersonalAccount/PersonalAccount";
 
 // ReviewsPage
-import PageReviews from "src/Pages/Reviews/Reviews";
-import PageTextReviews from "src/Pages/Reviews/TextReviews/TextReviews";
-import PagePhotoReviews from "src/Pages/Reviews/PhotoReviews/PhotoReviews";
+import PageReviews from "@pages/Reviews/Reviews";
+import PageTextReviews from "@pages/Reviews/TextReviews/TextReviews";
+import PagePhotoReviews from "@pages/Reviews/PhotoReviews/PhotoReviews";
 
 // AdminPanelPage
-import PageAdminPanel from "src/Pages/AdminPanel/AdminPanel";
-import PageChangeItems from "src/Pages/AdminPanel/ChangeItems/ChangeItems";
-import PageChangeAdditional from "src/Pages/AdminPanel/ChangeAdditional/ChangeAdditional";
-import PageChangeStocks from "src/Pages/AdminPanel/ChangeStocks/ChangeStocks";
+import PageAdminPanel from "@pages/AdminPanel/AdminPanel";
+import PageChangeItems from "@pages/AdminPanel/ChangeItems/ChangeItems";
+import PageChangeAdditional from "@pages/AdminPanel/ChangeAdditional/ChangeAdditional";
+import PageChangeStocks from "@pages/AdminPanel/ChangeStocks/ChangeStocks";
 
 // OthersPage
-import PageCart from "src/Pages/Cart/Cart";
-import PagePlaceAnOrder from "src/Pages/PlaceAnOrder/PlaceAnOrder";
-import PageQuestion from "src/Pages/Questions/Questions";
-import PagePaymentMethods from "src/Pages/PaymentMethods/PaymentMethods";
-import PageLogin from "src/Pages/Login/Login";
-import PageContacts from "src/Pages/Contacts/Contacts";
-import PageCardProduct from "src/Pages/CardProduct/CardProduct";
-import PageStocks from "src/Pages/Stocks/Stocks";
+import PageCart from "@pages/Cart/Cart";
+import PagePlaceAnOrder from "@pages/PlaceAnOrder/PlaceAnOrder";
+import PageQuestion from "@pages/Questions/Questions";
+import PagePaymentMethods from "@pages/PaymentMethods/PaymentMethods";
+import PageLogin from "@pages/Login/Login";
+import PageContacts from "@pages/Contacts/Contacts";
+import PageCardProduct from "@pages/CardProduct/CardProduct";
+import PageStocks from "@pages/Stocks/Stocks";
 
 export const router = createBrowserRouter([
   {
